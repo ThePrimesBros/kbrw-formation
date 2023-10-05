5511a1fd4dfbb5a4dfa492a7478f223edc857759
Raphaël Bessonner <raphael.bessonnier@kbrwadventure.com>
2023-10-05 14:11:49 +0200

    Debut du chap4 sur webflow

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,15 +1,29 @@
+var path = require('path'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+    CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+
 module.exports = {
     entry: './script.js',
     mode: 'development',
     devtool: 'inline-source-map',
-    output: { filename: 'bundle.js' },
-    plugins: [],
+    output: {
+        path: path.join(__dirname, '../priv/static'),
+        filename: '[name].js',
+    },
+    optimization: {
+        splitChunks: { cacheGroups: { styles: { name: 'styles', test: /\.css$/, chunks: 'all', enforce: true } } },
+        minimizer: [`...`, new CssMinimizerPlugin()]
+    },
+    plugins: [new MiniCssExtractPlugin({ insert: "", filename: "[name].css" })],
     module: {
         rules: [
             {
                 test: /.js?$/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: {
+                        test: /\.(css)$/,
+                        use: [{ loader: MiniCssExtractPlugin.loader }, { loader: "css-loader" }]
+                    },
                     options: {
                         presets: [
                             ["@babel/preset-env", { "targets": "defaults" }],
