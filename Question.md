c6f901545481d67f3ad4a7278dfe5724d2d26eab
Raphaël Bessonner <raphael.bessonnier@kbrwadventure.com>
2023-10-10 15:36:11 +0200

    Fin chap4

--- Question.md
+++ Question.md
@@ -1,13 +1,7 @@
-    Why use an ETS table ?
-    - Permet de stocker efficacement la donnée et de pouvoir la partager entre les processus
-    What are the advantages of wrapping an ETS table inside a GenServer ?
-    - Permet de créé un logique métier pour accéder au donnée de la table ets
-    What is a Behaviour in Elixir ?
-    - Ensemble de fonction qu'un module peut/doit implementer
 
-Go further
-
-    What are the differences between a Protocol and a Behaviour ?
-    - Un behaviour est un ensemble de fonction api qu'un module doit implementer alors qu'un protocol est un ensemble d'implementation polymorphique qui permet d'ajouter/modifier des fonctions sans touché au code sources
-    In which cases would you want to use a Protocol ? a Behaviour ?
-    - On va utilisé les protocol lorsque l'on voudra travaillé avec les données pour modifier sans touché au code sources et sinon les behaviour
+    What use can we have for a Loader or a Modal ?
+    - Le loader peut etre utilisé comme animation d'attente le temps que celle-ci se charge. La modal est une pop up qui permet d'afficher les informations ou des formulares
+    Why do we use CSS classes ?
+    - On utilise les classes CSS car JSXZ les utilise et surtout car webflow est plus compatible avec les classes que les id
+    What is Webflow ?
+    - Webflow est un site qui permet de générer des site webs à partir de maquettes
--- web/app.js
+++ web/app.js
@@ -1,8 +1,8 @@
 require('!!file-loader?name=[name].[ext]!./index.html')
 /* required library for our React app */
-var ReactDOM = require('react-dom')
-var React = require("react")
-var createReactClass = require('create-react-class')
+let ReactDOM = require('react-dom')
+let React = require("react")
+let createReactClass = require('create-react-class')
 
 /* required css for our application */
 require('./tuto.webflow/css/orders.css');
@@ -10,9 +10,26 @@ require('./tuto.webflow/css/details.css');
 
 var Page = createReactClass({
     render() {
+        var orders = [
+            { remoteid: "000000189", custom: { customer: { full_name: "TOTO & CIE" }, billing_address: "Some where in the world" }, items: 2 },
+            { remoteid: "000000190", custom: { customer: { full_name: "Looney Toons" }, billing_address: "The Warner Bros Company" }, items: 3 },
+            { remoteid: "000000191", custom: { customer: { full_name: "Asterix & Obelix" }, billing_address: "Armorique" }, items: 29 },
+            { remoteid: "000000192", custom: { customer: { full_name: "Lucky Luke" }, billing_address: "A Cowboy doesn't have an address. Sorry" }, items: 0 },
+        ]
         return <JSXZ in="orders" sel=".container">
+            {
+                orders.map(order => (<JSXZ in="orders" sel=".list-2" key={order.remoteid}>
+                    <Z sel=".id-2">{order.remoteid}</Z>
+                    <Z sel=".name-2">{order.custom.customer.full_name}</Z>
+                    <Z sel=".adresse-2">{order.custom.billing_address}</Z>
+                    <Z sel=".items-2">{order.items}</Z>
+                </JSXZ>))
+            }
         </JSXZ>
     }
 })
 
-ReactDOM.render(<Page />, document.getElementById('root'));
+ReactDOM.render(
+    <Page />,
+    document.getElementById('root')
+);
